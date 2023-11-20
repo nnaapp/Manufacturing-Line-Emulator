45ee5a78870d41e1390742c2a450383734d60c7a
nnaapp <connor@jnj.org>
2023-11-20 16:37:57 -0600

    Basic timer setup for machines

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,46 @@
+#![allow(non_snake_case)]
+
+use std::time::{UNIX_EPOCH,SystemTime,Duration};
+
+struct Machine
+{
+    deltaTime: f64,
+    tickSpeed: i64,
+    failChance: f32,
+}
+impl Machine 
+{
+    fn new(tickSpeed: i64, failChance: f32) -> Self
+    {
+        return Self { deltaTime: 0.0, tickSpeed: tickSpeed, failChance: failChance };
+    }
+
+    fn update(&self)
+    {
+        println!("Update func");
+    }
+}
+
 fn main() 
 {
-    println!("Hello, world!");
+    let myMachine = Machine::new(5, 0.01);
+
+    let mut start = SystemTime::now();
+    let mut iterTime = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let mut prevTime = iterTime;
+    let mut deltaTime:u128 = 0;
+    loop
+    {   
+        start = SystemTime::now();
+        iterTime = start.duration_since(UNIX_EPOCH).expect("Time went backwards");     
+        deltaTime += iterTime.as_millis() - prevTime.as_millis();
+        
+        if deltaTime >= 1000
+        {
+            println!("Test");
+            deltaTime -= 1000;
+        }
+
+        prevTime = iterTime;
+    }
 }
