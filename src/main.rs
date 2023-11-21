fe415bd24f95964e2460bfd51ba54da0941d808c
nnaapp <connor@jnj.org>
2023-11-21 14:30:15 -0600

    Added basic failure chance

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(non_snake_case)]
 
 use std::time::{UNIX_EPOCH,SystemTime,Duration};
+extern crate rand;
+use rand::{Rng, rngs::ThreadRng};
 
 struct Machine
 {
@@ -15,10 +17,23 @@ impl Machine
         return Self { deltaTime: 0, tickSpeed, failChance };
     }
 
-    fn update(&mut self)
+    fn update(&mut self, deltaTime: u128, rng: &mut ThreadRng)
     {
+        self.deltaTime += deltaTime;
+        
+        // If it is not time to execute a tick, return
+        if self.deltaTime < self.tickSpeed
+        {
+            return;
+        }
+
+        // Execute a tick
         println!("Update func");
         self.deltaTime -= self.tickSpeed;
+        if rng.gen_range(0.0..1.0) <= self.failChance
+        {
+            println!("Failure");
+        }
     }
 }
 
@@ -26,20 +41,24 @@ fn main()
 {
     let mut myMachine = Machine::new(500, 0.01);
 
+    // Master random number generator, which is passed to machines to use for faults
+    let mut rng = rand::thread_rng();
+
+    // Start represents current SystemTime, 
+    // iter/prevTime represent milliseconds since epoch time for the current and previous iteration of loop,
+    // deltaTime represents milliseconds time between previous and current iteration of loop.
     let mut start = SystemTime::now();
     let mut iterTime:Duration = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
     let mut prevTime:Duration = iterTime;
+    let mut deltaTime:u128;
     loop
     {   
         start = SystemTime::now();
         iterTime = start.duration_since(UNIX_EPOCH).expect("Time went backwards");     
 
-        myMachine.deltaTime += iterTime.as_millis() - prevTime.as_millis();
-        
-        if myMachine.deltaTime >= myMachine.tickSpeed
-        {
-            myMachine.update();
-        }
+        deltaTime = iterTime.as_millis() - prevTime.as_millis();
+
+        myMachine.update(deltaTime, &mut rng);
 
         prevTime = iterTime;
     }
