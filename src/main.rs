cf4e281b3c70cdee99e25ba800c1e71ab8fdd87a
nnaapp <connor@jnj.org>
2024-01-22 13:48:25 -0600

    Added prototype of conveyor belt

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,13 @@ struct Machine
     tickSpeed: u128, // tickSpeed is in milliseconds, number of milliseconds between ticks
     failChance: f32,
     state: OPCState,
+    output: Option<Box<Belt>>,
 }
 impl Machine 
 {
     fn new(tickSpeed: u128, failChance: f32, initialState: OPCState) -> Self
     {
-        return Self { deltaTime: 0, tickSpeed, failChance, state: initialState };
+        return Self { deltaTime: 0, tickSpeed, failChance, state: initialState, output: None };
     }
 
     fn update(&mut self, deltaTime: u128, seed: i32)
@@ -48,6 +49,11 @@ impl Machine
     fn producing(&mut self, seed: i32)
     {
         println!("Producing.");
+        if let Some(belt) = &mut self.output
+        {
+            belt.push(1);
+            println!("{}", belt.count);
+        }
         // Modulo seed by 1000, convert to float, convert to % (out of 1000), and compare to fail chance
         if (seed % 1000) as f32 / 1000.0 <= self.failChance
         {
@@ -64,9 +70,64 @@ impl Machine
     }
 }
 
+struct Belt
+{
+    capacity: i32,
+    count: i32,
+    output: Option<Box<Machine>>,
+}
+impl Belt
+{
+    fn new(capacity: i32) -> Self
+    {
+        return Self { capacity, count: 0, output: None };
+    }
+
+    // True = not full, False = full
+    fn check_full(&self, amount: i32) -> bool
+    {
+        return self.count + amount <= self.capacity;
+    }
+
+    // True = not empty, False = empty
+    fn check_empty(&self, amount: i32) -> bool
+    {
+        return self.count - amount >= 0;
+    }
+
+    // Returns the overflow, items that would not fit on the belt
+    fn push(&mut self, amount: i32) -> i32
+    {
+        if self.check_full(amount)
+        {
+            self.count += amount;
+            return 0;
+        }
+        
+        let overflow = (self.count + amount) - self.capacity;
+        self.count += amount - overflow;
+        return overflow;
+    }
+
+    // Returns the amount of items popped off the belt
+    fn pop(&mut self, amount: i32) -> i32
+    {
+        if self.check_empty(amount)
+        {
+            self.count -= amount;
+            return amount;
+        }
+
+        let underflow = self.count;
+        self.count = 0;
+        return underflow;
+    }
+}
+
 fn main() 
 {
-    let mut myMachine = Machine::new(500, 0.05, OPCState::PRODUCING);
+    let mut myMachine = Machine::new(750, 0.05, OPCState::PRODUCING);
+    myMachine.output = Some(Box::new(Belt::new(5)));
 
     // Master random number generator, which is passed to machines to use for faults
     let mut rng = rand::thread_rng();
